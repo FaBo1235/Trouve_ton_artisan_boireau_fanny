45454437e88229cca6324250f92866962306be98
Fanny BOIREAU <fannyboireau@MacBook-Pro-de-Fanny.local>
2025-12-04 20:02:12 +0100

    frontend

--- frontend/src/pages/Error.jsx
+++ frontend/src/pages/Error.jsx
@@ -1,13 +1,17 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
+import error from "../assets/img/warning.png"
 
 export default function Error() {
   return (
-    <div className="text-center py-20">
-      <h1 className="text-3xl text-blue-700 font-bold">Aucun résultat trouvé</h1>
-      <p className="text-gray-600 mt-2">
-        Essayez un autre mot clé ou une orthographe différente.
-      </p>
+    <div className="min-h-screen flex flex-col items-center justify-center text-center p-6">
+      <h1 className="text-3xl text-[#82B864] font-bold">Oopsie! Page non trouvée </h1>
+      <img src={error} alt="erreur 404" className="w-64 h-64 my-6" />
+      <button>
+        <Link to="/" className="bg-[#82B864]- text-[#0074C7]/50 px-4 py-2 rounded-xl hover:bg-[#82B864]">
+          Retour à l'accueil
+        </Link>
+      </button>
     </div>
   );
 }
